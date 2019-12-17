60017d0c16ed17b6be24562cd45df7b20c2cc9c9
AnonymityMaster <amasterx183@gmail.com>
2019-12-17 23:02:35 +0800

    hello world & update .gitignore file.

---
+++ src/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  gmchain-core
+//
+//  Created by AnonymityMaster on 2019/12/17.
+//  Copyright © 2019 ci6. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, ci6!\n");
+    return 0;
+}
